a3be0896d8f424706172ed422c927aa461a9353c
xaviguasch <xaviguasch@gmail.com>
2021-01-07 17:25:22 +0100

    solved exercise

---
+++ main.py
@@ -0,0 +1,11 @@
+def combine_words(word, **kwargs):
+    if "prefix" in kwargs:
+        return kwargs["prefix"] + word
+    elif "suffix" in kwargs:
+        return word + kwargs["suffix"]
+    return word
+
+
+print(combine_words("child", prefix="man"))
+print(combine_words("child", suffix="ish"))
+print(combine_words("child"))
